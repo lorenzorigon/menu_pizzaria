ce0cdd18e5b7e96941969f229ebcc39037f7dc45
Lorenzo Cardoso <lorenzocardoso@hotmail.com>
2024-05-08 07:36:05 -0300

    ajustes em produtos

--- app/Http/Controllers/CategoryController.php
+++ app/Http/Controllers/CategoryController.php
@@ -20,7 +20,7 @@ class CategoryController extends Controller
     }
     public function index() : View
     {
-        $categories = Category::paginate(10);
+        $categories = Category::query()->orderBy('position', 'asc')->paginate(10);
         return view("admin.category.index", compact("categories"));
     }
 
--- routes/web.php
+++ routes/web.php
@@ -2,6 +2,7 @@
 
 use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\CategoryController;
+use App\Http\Controllers\LoginController;
 use App\Http\Controllers\ProductController;
 
 Route::get('/', function () {
@@ -9,6 +10,10 @@ Route::get('/', function () {
 });
 
 Route::group(['prefix' => 'admin', 'as' => 'admin.'], function(){
+    Route::get('/login', [LoginController::class, 'showLoginForm'])->name('login');
+    Route::post('/login', [LoginController::class, 'login']);
+
+
     Route::group(['prefix' => 'categories', 'as' => 'categories.'], function () {
         Route::get('/', [CategoryController::class, 'index'])->name('index');
         Route::get('/create', [CategoryController::class, 'create'])->name('create');
